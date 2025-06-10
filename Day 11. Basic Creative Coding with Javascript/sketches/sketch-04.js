80c73dd65f2f5f5f0ce36eff8e4ed98ba14731f6
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2025-06-10 23:12:46 +0530

    feat: update sketch-04 to use a 10x10 grid and incorporate noise-based transformations

--- Day 11. Basic Creative Coding with Javascript/sketches/sketch-04.js
+++ Day 11. Basic Creative Coding with Javascript/sketches/sketch-04.js
@@ -1,4 +1,5 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -9,12 +10,12 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 4;
-    const rows = 4;
+    const cols = 10;
+    const rows = 10;
     const numCells = cols * rows;
 
-    const gridw = width / 0.8;
-    const gridh = height / 0.8;
+    const gridw = width * 0.8;
+    const gridh = height * 0.8;
     const cellw = gridw / cols;
     const cellh = gridh / rows;
 
@@ -30,9 +31,17 @@ const sketch = () => {
       const h = cellh * 0.8;
       const w = cellw * 0.8;
 
+      const n = random.noise2D(x, y, 0.001);
+      const angle = n * Math.PI * 0.2;
+      const scale = ((n + 1) / 2) * 30;
+
       context.save();
       context.translate(x, y);
       context.translate(margx, margy);
+      context.translate(cellw * 0.5, cellh * 0.5);
+      context.rotate(angle);
+
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
@@ -40,10 +49,6 @@ const sketch = () => {
       context.stroke();
 
       context.restore();
-
-      context.fillRect(x, y, cellw, cellh);
-
-      context.fillStyle = "white";
     }
   };
 };
