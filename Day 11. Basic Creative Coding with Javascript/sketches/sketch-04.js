63af0c42548dca9db76f9af4b107cfffc5edb2a4
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2025-06-13 23:57:41 +0530

    fix: replace Tweakpane with ControlKit for GUI controls and update parameters for improved functionality

--- Day 11. Basic Creative Coding with Javascript/sketches/sketch-04.js
+++ Day 11. Basic Creative Coding with Javascript/sketches/sketch-04.js
@@ -1,7 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
 const math = require("canvas-sketch-util/math");
-const Tweakpane = require("tweakpane");
+const ControlKit = require("controlkit");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -11,15 +11,18 @@ const settings = {
 const params = {
   cols: 10,
   rows: 10,
-  scaleMin: 1,
+  scaleMin: 0.1,
   scaleMax: 30,
   freq: 0.001,
-  amp: 0.2,
+  amp: 0.5,
   frame: 0,
+  speed: 10,
   animate: true,
+  lineCap: "round",
 };
 
 const sketch = () => {
+  createGUI();
   return ({ context, width, height, frame }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
@@ -32,63 +35,56 @@ const sketch = () => {
     const gridh = height * 0.8;
     const cellw = gridw / cols;
     const cellh = gridh / rows;
+    const margx = (width - gridw) / 2;
+    const margy = (height - gridh) / 2;
 
-    const margx = (width - gridw) * 0.5;
-    const margy = (height - gridh) * 0.5;
-
-    for (i = 0; i < numCells; i++) {
+    for (let i = 0; i < numCells; i++) {
       const col = i % cols;
       const row = Math.floor(i / cols);
 
       const x = col * cellw;
       const y = row * cellh;
-      const h = cellh * 0.8;
-      const w = cellw * 0.8;
 
       const f = params.animate ? frame : params.frame;
-
-      // const n = random.noise2D(x + frame * 50, y, params.freq);
-      const n = random.noise3D(x, y, f * 10, y, params.freq);
-
+      const n = random.noise3D(x, y, f * params.speed, params.freq);
       const angle = n * Math.PI * params.amp;
-      // const scale = ((n + 1) / 2) * 30;
-      // const scale = (n * 0.5 + 0.5) * 30;
       const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       context.save();
-      context.translate(x, y);
-      context.translate(margx, margy);
-      context.translate(cellw * 0.5, cellh * 0.5);
+      context.translate(x + margx + cellw / 2, y + margy + cellh / 2);
       context.rotate(angle);
-
       context.lineWidth = scale;
+      context.lineCap = params.lineCap;
 
       context.beginPath();
-      context.moveTo(w * -0.5, 0);
-      context.lineTo(w * 0.5, 0);
+      context.moveTo(-cellw * 0.4, 0);
+      context.lineTo(cellw * 0.4, 0);
       context.stroke();
-
       context.restore();
     }
   };
 };
 
-const createPane = () => {
-  const pane = new Tweakpane.pane();
-  let folder;
-
-  folder = pane.addFolder({ title: "Grid" });
-  folder.addInput(params, "cols", { min: 2, max: 50, step: 1 });
-  folder.addInput(params, "rows", { min: 2, max: 50, step: 1 });
-  folder.addInput(params, "scaleMin", { min: 1, max: 100 });
-  folder.addInput(params, "scaleMax", { min: 1, max: 100 });
-
-  folder = pane.addFolder({ title: "Noise" });
-  folder.addInput(params, "freq", { min: -0.01, max: 0.01 });
-  folder.addInput(params, "amp", { min: 0, max: 1 });
-  folder.addInput(params, "animate");
-  folder.addInput(params, "frame", { min: 0, max: 999 });
+const createGUI = () => {
+  const gui = new ControlKit();
+  gui
+    .addPanel({ label: "Grid Controls" })
+    .addGroup({ label: "Grid" })
+    .addSelector(params, "lineCap", ["butt", "round", "square"])
+    .addSlider(params, "cols", "range", { min: 5, max: 50, step: 1 })
+    .addSlider(params, "rows", "range", { min: 5, max: 50, step: 1 })
+    .addSlider(params, "scaleMin", "range", { min: 0.1, max: 100 })
+    .addSlider(params, "scaleMax", "range", { min: 1, max: 100 })
+    .addGroup({ label: "Noise" })
+    .addSlider(params, "freq", "range", {
+      min: 0.0001,
+      max: 0.01,
+      step: 0.0001,
+    })
+    .addSlider(params, "amp", "range", { min: 0, max: 1, step: 0.01 })
+    .addSlider(params, "frame", "range", { min: 0, max: 999, step: 1 })
+    .addSlider(params, "speed", "range", { min: 1, max: 100, step: 1 })
+    .addCheckbox(params, "animate");
 };
-createPane();
 
 canvasSketch(sketch, settings);
